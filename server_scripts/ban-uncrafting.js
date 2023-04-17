ec3fdd99f100181d9d921e6d7cf0eec819c73e64
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-04-17 12:44:35 +0800

    Unbanban need

--- server_scripts/ban-uncrafting.js
+++ server_scripts/ban-uncrafting.js
@@ -2,9 +2,9 @@
 	onEvent('block.right_click', event => {
 		if([
 			"twilightforest:uncrafting_table",
-			'mekanismgenerators:reactor_glass'
+			'-mekanismgenerators:reactor_glass'
 		].indexOf(event.block.getId()) != -1 ||
-		event.block.getId().startsWith('mekanismgenerators:fission_reactor_')){
+		event.block.getId().startsWith('-mekanismgenerators:fission_reactor_')){
 			event.cancel();
 		}
 	})

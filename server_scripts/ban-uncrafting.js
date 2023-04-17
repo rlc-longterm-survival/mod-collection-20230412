9882af7500c8236689450023c62217d64b41cd46
yezhiyi9670 <yezhiyi@toxic2018.ml>
2023-04-17 16:03:32 +0800

    Revert ban ban need

--- server_scripts/ban-uncrafting.js
+++ server_scripts/ban-uncrafting.js
@@ -1,10 +1,6 @@
 (() => {
 	onEvent('block.right_click', event => {
-		if([
-			"twilightforest:uncrafting_table",
-			'-mekanismgenerators:reactor_glass'
-		].indexOf(event.block.getId()) != -1 ||
-		event.block.getId().startsWith('-mekanismgenerators:fission_reactor_')){
+		if(event.block.getId() == "twilightforest:uncrafting_table"){
 			event.cancel();
 		}
 	})
